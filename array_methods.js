188c4ec706c72e1a3b08d49f46fc03e3c53c1f2e
umidevop <umidevop@sysXcall>
2023-09-03 22:54:20 +0500

    new tutorial

---
+++ array_methods.js
@@ -0,0 +1,33 @@
+// splice() method 
+
+// You can insert, remove, replace elements in array with splice method
+// You can delete item with splice(a,b); a is index for beginning and b is numbers to delete 
+
+let arr = [1,2,3,4,5]
+arr.splice(1,1) // it removes 1 item from index 1
+console.log(arr) // output > [1,3,4,5]
+
+
+// Deleting items with delete, but it doesn't change length of array 
+//Example 
+
+delete arr[5]
+
+console.log('length: ' + arr.length) // output > 5 
+
+
+// You can add items instead of removed items with splice()
+//arr.splice(a,b, item1, item2);
+
+arr.splice(1,2, 'hello' , 'world') // output > [1,'hello', 'world', 5]
+// splice method returns removed items
+arr = ["I", "study", "JavaScript", "right", "now"];
+
+// remove 2 first elements
+let removed = arr.splice(0, 2);
+
+console.log( removed ); // "I", "study" <-- array of removed elements
+
+// you can insert elemets without removing 
+
+  
